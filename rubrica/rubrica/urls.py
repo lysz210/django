c98b25f3cb35a8c4b5729053209c0c96cba2f4a3
Sergio Mattiello <sergio.mattiello@gmail.com>
2013-05-22 15:44:41 +0200

    template e view di esempio
    Un paio di esempi di template e view, funziona anche la gestione delle
    immagini statiche

--- rubrica/rubrica/urls.py
+++ rubrica/rubrica/urls.py
@@ -14,4 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+	url(r'^rubrica/$',"contatti.views.elenco"),
+    url(r'^contatto/(\d+)/$',"contatti.views.dettagli"),
+    
 )
